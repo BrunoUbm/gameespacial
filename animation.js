a19f3b92eec1898ce8338aff35f156b0b92d32ed
Bruno Ubaldo <brunoubaldo10@gmail.com>
2023-06-09 01:41:47 -0300

    remove button start

--- animation.js
+++ animation.js
@@ -1,96 +1,92 @@
-const StartButton = document.querySelector('.btn')
 const ElementInitial = document.querySelector('.initial')
 
-StartButton.addEventListener('click', () => {
-    ElementInitial.style.display = 'none'
-    class RocketPosition {
-        constructor(position, width, tiro, height, X, points) {
-            this.position = position
-            this.width = width
-            this.tiro = tiro
-            this.height = height
-            this.x = X
-            this.points = points
-        }  
-        MovementRocket() {
-            window.addEventListener('keypress', (event) => {
-                const keycode = event.charCode
-                const MarginLeft = String.fromCharCode(keycode)
-                switch (MarginLeft) {
-                    case 'd':
-                        this.position += 30
-                        if (this.position != this.width - 100) {
-                            const maxwidthRight = (this.width - 100) / this.position
-                            if (maxwidthRight < 1) {
-                                this.position = 0
-                            }
-                            document.querySelector('#foguete').style.left = `${this.position}px`
+class RocketPosition {
+    constructor(position, width, tiro, height, X, points) {
+        this.position = position
+        this.width = width
+        this.tiro = tiro
+        this.height = height
+        this.x = X
+        this.points = points
+    }  
+    MovementRocket() {
+        window.addEventListener('keypress', (event) => {
+            const keycode = event.charCode
+            const MarginLeft = String.fromCharCode(keycode)
+            switch (MarginLeft) {
+                case 'd':
+                    this.position += 30
+                    if (this.position != this.width - 100) {
+                        const maxwidthRight = (this.width - 100) / this.position
+                        if (maxwidthRight < 1) {
+                            this.position = 0
                         }
-                        break;
-    
-                    case 'a':
-                        this.position -= 30
-                        if (this.position != this.width) {
-                            const maxwidthLeft = this.width / this.position
-                            if (maxwidthLeft < 1) {
-                                this.position = this.width - 100
-                            }
-                            document.querySelector('#foguete').style.left = `${this.position}px`
-                        }
-                        break;
-                    
-                    case ' ':
-                            const elementTiro = document.querySelector('#tiro')
-                            const foguete = document.querySelector('#foguete')
-                            const alvo = document.querySelector('.alvo')
-                            const points = document.querySelector('.points')
+                        document.querySelector('#foguete').style.left = `${this.position}px`
+                    }
+                    break;
 
-                            if (this.tiro < this.height - 30) {
-                                const timeMov = setInterval(() => {
-                                    this.tiro += 5
-                                    elementTiro.style.bottom = `${this.tiro}px`
-                                    if (this.tiro >= this.height) {
-                                        const colision = (foguete.offsetLeft + 25) - alvo.offsetLeft
-                                        console.log(colision);
-                                        if (colision >= -40 && colision <= 40) {
-                                            this.points += 1
-                                            points.innerText = this.points
-                                        }
-                                        this.tiro = 100
-                                        clearInterval(timeMov)
+                case 'a':
+                    this.position -= 30
+                    if (this.position != this.width) {
+                        const maxwidthLeft = this.width / this.position
+                        if (maxwidthLeft < 1) {
+                            this.position = this.width - 100
+                        }
+                        document.querySelector('#foguete').style.left = `${this.position}px`
+                    }
+                    break;
+                
+                case ' ':
+                        const elementTiro = document.querySelector('#tiro')
+                        const foguete = document.querySelector('#foguete')
+                        const alvo = document.querySelector('.alvo')
+                        const points = document.querySelector('.points')
+
+                        if (this.tiro < this.height - 30) {
+                            const timeMov = setInterval(() => {
+                                this.tiro += 5
+                                elementTiro.style.bottom = `${this.tiro}px`
+                                if (this.tiro >= this.height) {
+                                    const colision = (foguete.offsetLeft + 25) - alvo.offsetLeft
+                                    console.log(colision);
+                                    if (colision >= -40 && colision <= 40) {
+                                        this.points += 1
+                                        points.innerText = this.points
                                     }
-                                }, 0.1);
-                            }
-                            break
-    
-                    default:
+                                    this.tiro = 100
+                                    clearInterval(timeMov)
+                                }
+                            }, 0.1);
+                        }
                         break
-                }
-            })
-                
-        }
-        MovementAlvo(){
-            const points = document.querySelector('.points')
-            const mov = setInterval(() => {
-                const alvo = document.querySelector('.alvo')
-                const RandomX = Math.floor(Math.random() * this.x)
-                alvo.style.left = `${RandomX}px`
-                
-            }, 2500)
-            setInterval(() => {
-                ElementInitial.style.display = "block"
-                document.querySelector('.text-initial').innerText = `A sua pontuação foi de ${this.points} pontos`
-                clearInterval(mov)
-                this.points = 0
-                points.innerText = this.points
 
-            }, 10000)
-        }
+                default:
+                    break
+            }
+        })
+            
     }
-    const PositionInitial = new RocketPosition(0, window.innerWidth, 100, window.innerHeight, window.innerWidth - 50, 0)
-    PositionInitial.MovementRocket()
-    PositionInitial.MovementAlvo()
-})
+    MovementAlvo(){
+        const points = document.querySelector('.points')
+        const mov = setInterval(() => {
+            const alvo = document.querySelector('.alvo')
+            const RandomX = Math.floor(Math.random() * this.x)
+            alvo.style.left = `${RandomX}px`
+            
+        }, 2500)
+        setInterval(() => {
+            ElementInitial.style.display = "block"
+            document.querySelector('.text-initial').innerText = `A sua pontuação foi de ${this.points} pontos`
+            clearInterval(mov)
+            this.points = 0
+            points.innerText = this.points
+
+        }, 10000)
+    }
+}
+const PositionInitial = new RocketPosition(0, window.innerWidth, 100, window.innerHeight, window.innerWidth - 50, 0)
+PositionInitial.MovementRocket()
+PositionInitial.MovementAlvo()
 
 
 
